b884bcb7ef6e5d3a9a9ef2b26c861020ff0bf314
Abhinav Yadav <141941530+Abhinavyadav01@users.noreply.github.com>
2025-10-05 00:15:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:"/abhinav-data-greens/",
   server: {
     host: "::",
     port: 8080,
